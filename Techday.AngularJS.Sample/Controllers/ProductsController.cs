af573ead725b0ed2e2aa826f2850aaad1a6c023b
Kirk Chen <kirkchen@nine-yi.com>
2014-09-09 18:30:06 +0800

    Refactor Code

---
+++ Techday.AngularJS.Sample/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Techday.AngularJS.Sample.Models;
+
+namespace Techday.AngularJS.Sample.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET: api/Blogs
+        public IEnumerable<Product> GetProducts()
+        {
+            var productList = new List<Product>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                productList.Add(new Product()
+                {
+                    title = "產品" + i,
+                    image = "http://lorempixel.com/200/200/sports/" + i,
+                    desc = "產品描述" + i
+                });
+            }
+
+            return productList;
+        }
+    }
+}
---
+++ Techday.AngularJS.Sample/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Techday.AngularJS.Sample.Models
+{
+    public class Product
+    {
+        public string image { get; set; }
+
+        public string title { get; set; }
+
+        public string desc { get; set; }
+    }
+}
