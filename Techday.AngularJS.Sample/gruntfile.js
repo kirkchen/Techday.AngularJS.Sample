6bc998effe6b1a90ecef1e25bcb03ebd0092301d
Kirk Chen <kirkchen@nine-yi.com>
2014-09-09 18:01:59 +0800

    add grunt automation

---
+++ Techday.AngularJS.Sample/gruntfile.js
@@ -0,0 +1,24 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({        
+        protractor: {
+            e2e: {
+                options: {
+                    configFile: 'protractor.conf.js',
+                    keepAlive: true
+                }
+            },
+            debug: {
+                options: {
+                    configFile: 'protractor.conf.js',
+                    keepAlive: true,
+                    debug: true
+                }
+            }
+        },
+    });
+
+    //add grunt protractor npm tasks into grunt.
+    grunt.loadNpmTasks('grunt-protractor-runner');
+
+    grunt.registerTask('default', ['protractor:e2e']);
+}
